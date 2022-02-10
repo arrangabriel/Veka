d763d0fca69a6d2394fc9bfd5af0b3426de8e1e7
Sander Boye Eikeland <sandee@stud.ntnu.no>
2022-02-10 17:42:00 +0100

    start log in page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import Innlegg from './components/Innlegg/Innlegg'
+import LoginForm from './components/Innlegg/LoginForm';
 
 function App() {
   return (
     <div>
-      
+      <LoginForm></LoginForm>
     </div>
   );
 }

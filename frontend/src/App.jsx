4c8b7739c4d9da0ff8c599d4ef3d42bc483f0274
David Lekve <davidlek@stud.ntnu.no>
2022-02-15 12:36:33 +0100

    enable use of bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
 import Innlegg from './components/Innlegg/Innlegg'
 import LoginForm from './components/Innlegg/LoginForm';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
-      <LoginForm></LoginForm>
+      {/* <LoginForm></LoginForm> */}
+      <Button variant="secondary">My button</Button>
     </div>
   );
 }

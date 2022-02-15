72c1938249791a8c18eaaa0498024e1cd46cee0a
Selma Hidle <selmahi@stud.ntnu.no>
2022-02-15 13:44:14 +0100

    Update profile with user attributes in the api

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
-from rest_framework import viewsets
 from .serializers import ProfileSerializer
 from .models import Profile
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
+@api_view(['GET', 'POST'])
+def profiles_list(request):
+    if request.method == 'GET':
+        data = Profile.objects.all()
+        serializer = ProfileSerializer(data, context={'request': request}, many=True)
+        return Response(serializer.data)
 
-class ProfileView(viewsets.ModelViewSet):
-    serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    elif request.method == 'POST':
+        serializer = ProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def students_detail(request, pk):
+    try:
+        profile = Profile.objects.get(pk=pk)
+    except Profile.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = ProfileSerializer(profile, data=request.data,context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- backend/veka_project/urls.py
+++ backend/veka_project/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from users import views
 
-router = routers.DefaultRouter()
-router.register(r'profiles', views.ProfileView, 'profile')
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    re_path(r'^api/profiles/', views.profiles_list)
 ]

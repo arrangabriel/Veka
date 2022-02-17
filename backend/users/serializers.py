b1f2bdca160e6931121fee71e5572096de9f8bcb
Selma Hidle <selmahi@stud.ntnu.no>
2022-02-17 12:28:15 +0100

    Update serializer with exclude

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('__all__')
+        exclude = ('id', 'last_login', 'is_superuser', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined', 'groups', 'user_permissions', )
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=True)

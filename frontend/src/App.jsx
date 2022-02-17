0710328428ffcde06c61f3f9de9c386189b3dfa0
Sander Boye Eikeland <sandee@stud.ntnu.no>
2022-02-17 16:11:49 +0100

    add card as listing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Popup from './components/Innlegg/Popup';
 import ListingView from './components/Innlegg/UserPost/ListingView'
+import Popup from './components/Innlegg/Popup';
 
 import LoginForm from './components/Innlegg/LoginForm';
 import SignUp from './components/Innlegg/SignUp';
@@ -25,40 +25,48 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>LoginForm</Link>
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/my-user"}>Min bruker</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/add-post"}>Nytt innlegg</Link>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-      <input
-      type="button"
-      value="Click to Open Popup"
-      onClick={togglePopup}
-      />
-
       {isOpen && <Popup
       content={<>
         <UserPost></UserPost>
       </>}
       handleClose={togglePopup}
-    />}
+      />}
 
-      <div className="auth-wrapper">
+      <div className="auth-wrapper">  
         <div className="auth-inner">
           <Routes>
             <Route exact path='/' element={<ListingView/>} />
             <Route path="/sign-in" element={<LoginForm/>} />
             <Route path="/sign-up" element={<SignUp/>} />
             <Route path="/add-post" element={<UserPost/>} />
+            <Route path="/my-user" element={LoginForm} />
+
           </Routes>
         </div>
+        <input
+          type="button"
+          value="Klikk her for å legge ut innlegg"
+          onClick={togglePopup}
+        />
       </div>
-    </div></Router>
+    </div>
+    </Router>
   );
 }
 

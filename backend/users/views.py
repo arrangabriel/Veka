0e52e9d4b5ba499bcbaf110baf431fe87369b0fb
Selma Hidle <selmahi@stud.ntnu.no>
2022-02-24 14:27:42 +0100

    Update ViewSets to ModelsViewSets

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,4 @@
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer, UserSerializer
 from .models import Profile
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -7,36 +7,63 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.core.exceptions import ValidationError
 from django.contrib.auth import authenticate, login, logout
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 
-class ProfilesViewSet(viewsets.ViewSet):
-    
-    def list(self, request):
-        queryset = Profile.objects.all()
-        serializer = ProfileSerializer(queryset, many=True)
-        return Response(serializer.data)
+class ProfilesViewSet(viewsets.ModelViewSet):
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'list':
+            permission_classes = [IsAuthenticated]
+        elif self.action == 'create':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
     def create(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        email = request.data.get('email')
+        username = request.POST['user.username']
+        password = request.POST['user.password']
+        email = request.POST['user.email']
         try:
-            User.objects.create_user(username=username, email=email, password=password).save()
+            user = User.objects.create_user(username=username, email=email, password=password).save()
             return Response(status=status.HTTP_201_CREATED)
         except IntegrityError:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-class LoginViewSet(viewsets.ViewSet):
+class LoginViewSet(viewsets.ModelViewSet):
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'list' or self.action == 'create':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
     def create(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
+        username = request.POST['username']
+        password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
             return Response(status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    def list(self, request):
+        return Response(status=status.HTTP_200_OK)
 
 
 class LogoutViewSet(viewsets.ViewSet):
@@ -48,9 +75,23 @@ class LogoutViewSet(viewsets.ViewSet):
 
 class EditViewSet(viewsets.ViewSet):
 
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'list':
+            permission_classes = [AllowAny]
+        elif self.action == 'create':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+    serializer = ProfileSerializer
+
     def create(self, request):
-        bio = request.data.get('bio')
-        location = request.data.get('location')
+        bio = request.POST['bio']
+        location = request.POST['location']
         if request.user.is_authenticated:
             profile = Profile.objects.get(user=request.user.id)
             if (bio):

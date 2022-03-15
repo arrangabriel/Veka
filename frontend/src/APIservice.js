7a9d709d4a4d8788cc79cb744470e5234066d79b
Selma Hidle <selmahi@stud.ntnu.no>
2022-03-15 16:24:56 +0100

    start connecting frontend and backend to save user posting

--- frontend/src/APIservice.js
+++ frontend/src/APIservice.js
@@ -1,25 +1,42 @@
-export default class APIservice{
-    static Login(body){
-        return(fetch('http://127.0.0.1:8000/auth/', {
-              method:'POST',
-              headers:{
-                Accept: 'application/json',
-                'Content-type':'application/json',
-              },
-              credentials:'include',
-              body: JSON.stringify(body)
-            }))
-    }
 
-    static Signup(body){
-      return(fetch('http://127.0.0.1:8000/api/profiles/', {
-              method:'POST',
-              headers:{
-                Accept: 'application/json',
-                'Content-type':'application/json',
-              },
-              // credentials:'include',
-              body: JSON.stringify(body)
-            }))
-    }
+
+export default class APIservice {
+  static Login(body) {
+    return (fetch('http://127.0.0.1:8000/auth/', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(body)
+    }))
+  }
+
+  static Signup(body) {
+    return (fetch('http://127.0.0.1:8000/api/profiles/', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json',
+      },
+      // credentials:'include',
+      body: JSON.stringify(body)
+    }))
+  }
+
+
+  static CreateListings(body, token) {
+    console.log(token)
+    return (fetch('http://127.0.0.1:8000/api/listings/', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Authorization': 'Token ' + token.mytoken,
+        'Content-type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(body)
+    }))
+  }
 }

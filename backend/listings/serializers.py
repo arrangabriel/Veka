68b359f270fc83ce4813398d464bbad4cfc0c524
Arran Ø. K. Gabriel <arrankg@gmail.com>
2022-03-22 15:06:53 +0100

    Slug interested_users field to only be id's

--- backend/listings/serializers.py
+++ backend/listings/serializers.py
@@ -4,15 +4,14 @@ from .models import Listing
 
 
 class ListingReadSerializer(serializers.ModelSerializer):
-    #owner = ProfileSerializer()
     username = serializers.ReadOnlyField(source='owner.user.username')
-    #interested = serializers.(source='interested_users')
+    interested_users = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field='id')
 
     class Meta:
         model = Listing
-        # TODO this needs to return ID
         fields = ('id', 'owner', 'username', 'title', 'description',
-                  'date', 'location', 'listing_type', 'event_type', 'price')
+                  'date', 'location', 'listing_type', 'event_type', 'price', 'interested_users')
         depth = 1
 
 
